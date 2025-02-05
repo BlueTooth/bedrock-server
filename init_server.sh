e3a3b77b6eec11f69f1807a3279af5046d43f926
BlueTooth <BlueTooth@users.noreply.github.com>
2025-02-05 12:59:53 +0100

    Update init_server.sh
    remove wget

--- init_server.sh
+++ init_server.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-VERSION_INSTALLED=$(cat Dedicated_Server.txt | grep -oP 'Version: ([0-9.]*)' | grep -oP '[0-9.]*')
+VERSION_FILE=version.txt
+
+touch $VERSION_FILE
+VERSION_INSTALLED=$(head -n 1 $VERSION_FILE)
 echo installed bedrock-server-version $VERSION_INSTALLED
 
 echo search bedrock-server-version...
-VERSION=`curl -s -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" https://www.minecraft.net/en-us/download/server/bedrock | grep -oP '([0-9.])*(?=\.zip)' | head -n 1`
+VERSION=`curl -s -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" \
+ https://www.minecraft.net/en-us/download/server/bedrock | grep -oP '([0-9.])*(?=\.zip)' | head -n 1`
 echo found bedrock-server-version $VERSION
 
 if [ "$VERSION" = "" ]
@@ -15,7 +19,9 @@ then
  echo doing update $VERSION_INSTALLED to $VERSION
  
  echo download bedrock-server-$VERSION.zip...
- wget -q --output-document=bedrock-server.zip https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-$VERSION.zip
+ curl -s -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" \
+  -o bedrock-server.zip \
+  https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-$VERSION.zip
  if [ `wc -c <bedrock-server.zip` -ge 1000 ]
  then
   echo saved in /data/bedrock-server.zip
@@ -30,6 +36,8 @@ then
   
   echo get more rights to executable
   chmod 777 bedrock_server
+  
+  echo $VERSION > $VERSION_FILE
  else
   echo download bedrock-server-$VERSION.zip failed
  fi
