8c6ad2bfd55a4877985d6735a4cb063e6eedd6dd
BlueTooth <BlueTooth@users.noreply.github.com>
2025-02-07 12:27:21 +0100

    Update init_server.sh
    go bash

--- init_server.sh
+++ init_server.sh
@@ -1,4 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+
+curlArgs=('-s' '-H' "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36")
+
+downloadFile () {
+  echo "download fron $2..."
+  curl "${curlArgs[@]}" -o $1 $2
+  if [ `wc -c <$1` -le 1000 ]
+  then
+    echo "download $2 failed"
+  fi
+}
+
+unzipTo () {
+  echo "unzip to $2..."
+  if [ `wc -c <$1` -ge 1000 ]
+  then
+    unzip -oq $1 -d $2
+    echo "files unzipped to $2"
+  else
+    echo "zip-file to small"
+  fi
+}
+
+downloadUnzipAndRemove () {
+  downloadFile "file.zip" $1
+  unzipTo "file.zip" $2
+  rm -f "file.zip"
+}
+
 VERSION_FILE=version.txt
 
 touch $VERSION_FILE
@@ -6,56 +35,27 @@ VERSION_INSTALLED=$(head -n 1 $VERSION_FILE)
 echo installed bedrock-server-version $VERSION_INSTALLED
 
 echo search bedrock-server-version...
-VERSION=`curl -s -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" \
- https://www.minecraft.net/en-us/download/server/bedrock | grep -oP '([0-9.])*(?=\.zip)' | head -n 1`
+VERSION=`curl curl "${curlArgs[@]}" https://www.minecraft.net/en-us/download/server/bedrock | grep -oP '([0-9.])*(?=\.zip)' | head -n 1`
 echo found bedrock-server-version $VERSION
 
 if [ "$VERSION" = "" ]
 then
- echo version not available
+  echo version not available
 elif [ "$VERSION" != "$VERSION_INSTALLED" ]
 then
- echo newer version available...
- echo doing update $VERSION_INSTALLED to $VERSION
- 
- echo download bedrock-server-$VERSION.zip...
- curl -s -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" \
-  -o bedrock-server.zip \
-  https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-$VERSION.zip
- if [ `wc -c <bedrock-server.zip` -ge 1000 ]
- then
-  echo saved in /data/bedrock-server.zip
-
-  echo unzip bedrock-server.zip...
-  unzip -oq bedrock-server.zip
-  echo files unzipped in /data
-
-  echo remove bedrock-server.zip...
-  rm -f bedrock-server.zip
-  echo bedrock-server.zip removed
+  echo newer version available...
+  echo doing update $VERSION_INSTALLED to $VERSION
+  
+  downloadUnzipAndRemove "https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-$VERSION.zip" /data
   
   echo get more rights to executable
   chmod 777 bedrock_server
   
   echo $VERSION > $VERSION_FILE
-  
-  curl -o nvidia.zip https://images.nvidia.com/content/minecraft-with-rtx-beta-resource-packs/nvidia-pbr-example-texturesets-pixelart-feb-2-2021-final.mcpack
-  if [ `wc -c <nvidia.zip` -ge 1000 ]
-  then
-   unzip -oq nvidia.zip -d resource_packs/nvidia
-  fi
-  
-  curl -o vanilla-RTX.zip https://minecraftrtx.net/downloads/Vanilla-RTX-v1.21.80.mcpack
-  if [ `wc -c <vanilla-RTX.zip` -ge 1000 ]
-  then
-   unzip -oq vanilla-RTX.zip -d resource_packs/vanilla-RTX
-  fi
 
-  curl -o vanilla-RTX-Normals.zip https://minecraftrtx.net/downloads/Vanilla-RTX-Normals-v1.21.80.mcpack
-  if [ `wc -c <vanilla-RTX-Normal.zip` -ge 1000 ]
-  then
-   unzip -oq vanilla-RTX-Normals.zip -d resource_packs/vanilla-RTX-Normals
-  fi
+  downloadUnzipAndRemove "https://images.nvidia.com/content/minecraft-with-rtx-beta-resource-packs/nvidia-pbr-example-texturesets-pixelart-feb-2-2021-final.mcpack" /data/resource_packs/nvidia
+  downloadUnzipAndRemove "https://minecraftrtx.net/downloads/Vanilla-RTX-v1.21.80.mcpack" /data/resource_packs/vanilla-RTX
+  downloadUnzipAndRemove "https://minecraftrtx.net/downloads/Vanilla-RTX-Normals-v1.21.80.mcpack" /data/resource_packs/vanilla-RTX-Normals
   
   cat > world_resoure_packs.json <<EOF
 [
@@ -75,7 +75,7 @@ then
 EOF
 
  else
-  echo download bedrock-server-$VERSION.zip failed
+  echo download $1 failed
  fi
 fi
 
