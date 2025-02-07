3a8eb3b846c89458ca6ca7ba00e3e0762b5f4f6c
BlueTooth <BlueTooth@users.noreply.github.com>
2025-02-07 13:03:04 +0100

    Update init_server.sh
    bugfix

--- init_server.sh
+++ init_server.sh
@@ -58,7 +58,7 @@ then
   downloadUnzipAndRemove "https://minecraftrtx.net/downloads/Vanilla-RTX-Normals-v1.21.80.mcpack" /data/resource_packs/vanilla-RTX-Normals
 
   echo "add world_resoure_packs.json"
-  touch "worlds/Bedrock level/world_resoure_packs.json"
+  mkdir -p "worlds/Bedrock level"
   cat > "worlds/Bedrock level/world_resoure_packs.json" <<EOF
 [
  {
